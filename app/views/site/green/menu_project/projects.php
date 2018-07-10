a9dcb7f8e292048c577d7adf652d45c3e9037bcb
Titpanha <panhafree12@gmail.com>
2018-07-10 11:08:36 +0700

    padding

--- app/views/site/green/menu_project/projects.php
+++ app/views/site/green/menu_project/projects.php
@@ -21,29 +21,29 @@
 		</nav>
 		<div class="contentItem">
 			<div class="row">
-				<div class="col-md-3">
-					<a href=""><img class="image3" src="<?=base_url('assets/images/buildingInformatin.jpg')?>"></a>	
+				<div class="col-md-3 " style="padding-top: 20px;">
+					<a href="<?=base_url('site/church')?>"><img class="image3" src="<?=base_url('assets/images/church1.jpg')?>"></a>	
 				</div>
-				<div class="col-md-3">
-					<a href=""><img class="image3" src="<?=base_url('assets/images/buildingInformatin.jpg')?>"></a>	
+				<div class="col-md-3 " style="padding-top: 20px;">
+					<a href="<?=base_url('site/education')?>"><img class="image3" src="<?=base_url('assets/images/Arapahoe-HS-Library-1.jpg')?>"></a>	
 				</div>
-				<div class="col-md-3">
-					<a href=""><img class="image3" src="<?=base_url('assets/images/buildingInformatin.jpg')?>"></a>	
+				<div class="col-md-3 " style="padding-top: 20px;">
+					<a href="<?=base_url('site/goverment')?>"><img class="image3" src="<?=base_url('assets/images/DPKb4k_UQAAVyii.jpg')?>"></a>	
 				</div>
-				<div class="col-md-3">
-					<a href=""><img class="image3" src="<?=base_url('assets/images/buildingInformatin.jpg')?>"></a>	
+				<div class="col-md-3 " style="padding-top: 20px;">
+					<a href="<?=base_url('site/health_science')?>"><img class="image3" src="<?=base_url('assets/images/ERA-Lab-3-150x150.jpg')?>"></a>	
 				</div>
-				<div class="col-md-3">
-					<a href=""><img class="image3" src="<?=base_url('assets/images/buildingInformatin.jpg')?>"></a>	
+				<div class="col-md-3 " style="padding-top: 20px;">
+					<a href="<?=base_url('site/menufacture')?>"><img class="image3" src="<?=base_url('assets/images/BE-1900-Simulator-1-150x150.jpg')?>"></a>	
 				</div>
-				<div class="col-md-3">
-					<a href=""><img class="image3" src="<?=base_url('assets/images/buildingInformatin.jpg')?>"></a>	
+				<div class="col-md-3 " style="padding-top: 20px;">
+					<a href="<?=base_url('site/multifamily')?>"><img class="image3" src="<?=base_url('assets/images/Platform-Model-2BR-11-150x150.jpg')?>"></a>	
 				</div>
-				<div class="col-md-3">
-					<a href=""><img class="image3" src="<?=base_url('assets/images/buildingInformatin.jpg')?>"></a>	
+				<div class="col-md-3 " style="padding-top: 20px;">
+					<a href="<?=base_url('site/office')?>"><img class="image3" src="<?=base_url('assets/images/Belleview-Station-Meeting-Room-150x150.jpg')?>"></a>	
 				</div>
-				<div class="col-md-3">
-					<a href=""><img class="image3" src="<?=base_url('assets/images/buildingInformatin.jpg')?>"></a>	
+				<div class="col-md-3 " style="padding-top: 20px;">
+					<a href="<?=base_url('site/restaurant')?>"><img class="image3" src="<?=base_url('assets/images/marcos-pizza4-150x150.jpeg')?>"></a>	
 				</div>
 			</div>
 		</div>
